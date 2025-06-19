6fe5bfd8fa9cdf0c339d690dc49445e158a686d2
JosemasitoGamer <jose.soriano@live.u-tad.com>
2025-06-19 19:39:10 +0200

    remove bad include

--- Source/MPV_Practicas/PathFinder/Connection.cpp
+++ Source/MPV_Practicas/PathFinder/Connection.cpp
@@ -1,2 +1,1 @@
-#include "C:\Users\JOSE MANUEL\Desktop\UTAD\IA\MPV_Practicas_Skeleton\Practicas\UE_SteeringBehaviour\Intermediate\Build\Win64\x64\MPV_PracticasEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.RTTI.NoValFmtStr.ValApi.Cpp20.InclOrderUnreal5_3.h"
 #include "Connection.h"
